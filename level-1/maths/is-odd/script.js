9e183069ff406edf1ed7275bd5a5f47fc4fe8322
gregoriusgrd <gregorius.gerald21@gmail.com>
2025-03-13 00:36:00 +0700

    Initial commit

---
+++ level-1/maths/is-odd/script.js
@@ -0,0 +1,5 @@
+function isOdd(num) {
+  return num % 2 !== 0;
+}
+
+console.log(isOdd(-5)); // isOdd(-5) ➞ true
---
+++ level-1/string/number-and-string-conversion/script.js
@@ -0,0 +1,10 @@
+function intToString(num) {
+  return String(num);
+}
+
+function stringToInt(num) {
+  return Number(num);
+}
+
+console.log(intToString(4)); // ➞ "4"
+console.log(stringToInt("4")); // ➞ 4
