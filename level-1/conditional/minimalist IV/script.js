4cd25cfd6f99c062b294eba11f2a5f879ba66120
gregoriusgrd <gregorius.gerald21@gmail.com>
2025-03-13 01:50:02 +0700

    Create script.js

---
+++ level-1/conditional/minimalist IV/script.js
@@ -0,0 +1,21 @@
+// Minimalist version
+
+function equilibrium(x) {
+  if (x > 0) return "positive";
+  if (x < 0) return "negative";
+  return true;
+}
+
+// Normal version
+
+/*
+function equilbrium (x) {
+	if (x > 0) {
+		return "positive"
+	} else if (x < 0) {
+		return "negative"
+	} else {
+		return true
+	}
+}
+*/
