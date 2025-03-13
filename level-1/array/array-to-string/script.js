2dd70d611c644d734ae3f7d9217fdb49bb0d653d
gregoriusgrd <gregorius.gerald21@gmail.com>
2025-03-14 02:24:30 +0700

    Initial commit

---
+++ level-1/array/array-to-string/script.js
@@ -0,0 +1,6 @@
+function arrayToString(arr) {
+  return arr.join("").toString();
+}
+
+arrayToString([1, 2, 3, 4, 5, 6]); // ➞ "123456"
+arrayToString(["a", "b", "c", "d", "e", "f"]); // ➞ "abcdef"
---
+++ level-1/array/find-index/script.js
@@ -0,0 +1,7 @@
+function findIndex(arr, str) {
+  return arr.indexOf(str);
+}
+
+findIndex(["hi", "edabit", "fgh", "abc"], "fgh"); // ➞ 2
+
+findIndex(["Red", "blue", "Blue", "Green"], "blue"); // ➞ 1
