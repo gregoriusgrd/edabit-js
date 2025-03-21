164ea5234f2bb0730c7d52f25aca75db6f0f32af
gregoriusgrd <gregorius.gerald21@gmail.com>
2025-03-21 22:01:29 +0700

    Update script.js

--- level-1/array/format-date/script.js
+++ level-1/array/format-date/script.js
@@ -6,3 +6,10 @@ function formatDate(date) {
 }
 
 console.log(formatDate("11/12/2019")); //  ➞ "20191211"
+
+// minimalist version
+/*
+function formatDate(date) {
+	return date.split("/").reverse().join("")
+}
+*/
