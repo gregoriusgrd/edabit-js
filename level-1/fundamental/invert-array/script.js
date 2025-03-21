c3af260809e23412ea89973444ae5e2f47a280a0
gregoriusgrd <gregorius.gerald21@gmail.com>
2025-03-22 04:13:24 +0700

    Create script.js

---
+++ level-1/fundamental/invert-array/script.js
@@ -0,0 +1,18 @@
+// classic
+function negative(arr) {
+  let result = [];
+  for (let i = 0; i < arr.length; i++) {
+    result.push(arr[i] * -1);
+  }
+  return result;
+}
+
+console.log(negative([-1, -2])); // [1, 2]
+console.log(negative([3, -5, 7])); // [-3, 5, -7]
+
+// using arr.map()
+/*
+function negative(arr) {
+    return arr.map(num => -Math.abs(num));
+}
+*/
