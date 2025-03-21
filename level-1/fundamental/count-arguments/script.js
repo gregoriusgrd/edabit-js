fe80a871f6a8a51ddeabe23d59ee5f222f970755
gregoriusgrd <gregorius.gerald21@gmail.com>
2025-03-22 03:43:32 +0700

    Update script.js

--- level-1/fundamental/count-arguments/script.js
+++ level-1/fundamental/count-arguments/script.js
@@ -5,3 +5,14 @@ function numArgs(...args) {
   }
   return count;
 }
+
+numArgs(); // ➞ 0
+numArgs("foo"); // ➞ 1
+numArgs("foo", "bar"); // ➞ 2
+
+// minimalist
+/*
+function numArgs(...args) {
+	return args.length;
+}
+*/
