9512453b0d392f6bd9e9ff28b33e9aadcbbdd1fa
gregoriusgrd <gregorius.gerald21@gmail.com>
2025-03-25 22:53:11 +0700

    Create script.js

---
+++ level-1/string/slice-repeat/script.js
@@ -0,0 +1,7 @@
+function frontThree(str) {
+  return str.slice(0, 3).repeat(3);
+}
+
+frontThree("Python"); // ➞ "PytPytPyt"
+frontThree("Cucumber"); // ➞ "CucCucCuc"
+frontThree("bioshock"); // ➞ "biobiobio"
