f9d95f8c9cc9cedfd930a143277eab1c0da548db
gregoriusgrd <gregorius.gerald21@gmail.com>
2025-03-31 21:49:15 +0700

    Update script.js

--- level-1/string/case-sensitive-comparison/script.js
+++ level-1/string/case-sensitive-comparison/script.js
@@ -7,7 +7,10 @@ function match(s1, s2) {
 }
 
 match("hello", "hELLo"); // ➞ true
-
 match("motive", "emotive"); // ➞ false
-
 match("venom", "VENOM"); // ➞ true
+
+// minimalist (using ternary)
+/*
+const match = (s1, s2) => s1.toLowerCase() === s2.toLowerCase() ? true : false
+*/
