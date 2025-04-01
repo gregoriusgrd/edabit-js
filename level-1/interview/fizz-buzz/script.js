b5894053f3ad104cf9f161dee084dbe197b5cf3d
gregoriusgrd <gregorius.gerald21@gmail.com>
2025-04-01 19:29:39 +0700

    Update script.js

--- level-1/interview/fizz-buzz/script.js
+++ level-1/interview/fizz-buzz/script.js
@@ -14,3 +14,20 @@ FizzBuzz(3); // ➞ "Fizz"
 FizzBuzz(5); // ➞ "Buzz"
 FizzBuzz(15); // ➞ "FizzBuzz"
 FizzBuzz(4); // ➞ "4"
+
+// ternary
+/*
+function FizzBuzz(num) {
+	return num % 15 === 0 ? 'FizzBuzz' 
+	     : num % 5 === 0 ? 'Buzz' 
+	     : num % 3 === 0 ? 'Fizz' 
+	     : String(num);
+}
+*/
+
+// shortest
+/*
+function FizzBuzz(num) {
+	return (num%3?'':'Fizz')+(num%5?'':'Buzz')||`${num}`;
+}
+*/
