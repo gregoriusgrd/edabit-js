e3c8fe66994cce9edf31f9d3e4e828138e70ea8e
gregoriusgrd <gregorius.gerald21@gmail.com>
2025-04-03 01:14:14 +0700

    Create script.js

---
+++ level-1/fundamental/str-to-int-vice-versa/script.js
@@ -0,0 +1,12 @@
+function toInt(str) {
+  return parseInt(str);
+}
+
+function toStr(int) {
+  return int.toString();
+}
+
+toInt("77"); // ➞ 77
+toInt("532"); // ➞ 532
+toStr(77); // ➞ "77"
+toStr(532); // ➞ "532"
