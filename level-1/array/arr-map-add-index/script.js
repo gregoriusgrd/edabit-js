12efcaac42056146599c3fa34d6d4732e17cc915
gregoriusgrd <gregorius.gerald21@gmail.com>
2025-04-02 20:05:11 +0700

    Update script.js

--- level-1/array/arr-map-add-index/script.js
+++ level-1/array/arr-map-add-index/script.js
@@ -1,3 +1,7 @@
 function addIndexes(arr) {
   return arr.map((n, i) => n + i);
 }
+
+addIndexes([0, 0, 0, 0, 0]); // ➞ [0, 1, 2, 3, 4]
+addIndexes([1, 2, 3, 4, 5]); // ➞ [1, 3, 5, 7, 9]
+addIndexes([5, 4, 3, 2, 1]); // ➞ [5, 5, 5, 5, 5]
