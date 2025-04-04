270287e7ea2058b6c139620d5a97059531d024fa
gregoriusgrd <gregorius.gerald21@gmail.com>
2025-04-04 21:11:46 +0700

    Update script.js

--- level-1/array/sandwich/script.js
+++ level-1/array/sandwich/script.js
@@ -1,10 +1,15 @@
 // using pop and shift
 function getFillings(sandwich) {
-  let pop = sandwich.pop();
-  let shift = sandwich.shift();
+  sandwich.pop();
+  sandwich.shift();
   return sandwich;
 }
 
 getFillings(["bread", "ham", "cheese", "ham", "bread"]); // ➞ ["ham", "cheese", "ham"]
 getFillings(["bread", "sausage", "tomato", "bread"]); // ➞ ["sausage", "tomato"]
 getFillings(["bread", "lettuce", "bacon", "tomato", "bread"]); // ➞ ["lettuce", "bacon", "tomato"]
+
+// using slice
+/*
+const getFillings = sandwich => sandwich.slice(1, -1);
+*/
