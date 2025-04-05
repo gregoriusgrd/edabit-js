46672486fa908a20f769bbc8b8118d5cd396afc5
gregoriusgrd <gregorius.gerald21@gmail.com>
2025-04-05 21:12:41 +0700

    Update script.js

--- level-1/array/same-bread-shift-pop/script.js
+++ level-1/array/same-bread-shift-pop/script.js
@@ -1,3 +1,15 @@
 function hasSameBread(arr1, arr2) {
   return arr1.shift() === arr2.shift() && arr1.pop() === arr2.pop();
 }
+
+hasSameBread(
+  ["white bread", "lettuce", "white bread"],
+  ["white bread", "tomato", "white bread"]
+); // ➞ true
+
+hasSameBread(
+  ["brown bread", "chicken", "brown bread"],
+  ["white bread", "chicken", "white bread"]
+); // ➞ false
+
+hasSameBread(["toast", "cheese", "toast"], ["brown bread", "cheese", "toast"]); // ➞ false
